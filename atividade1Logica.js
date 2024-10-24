ff44e46f9b3ef63ad353031f5abdc496c4facebe
MARCELO VIEIRA <104174604+MARSELO10@users.noreply.github.com>
2024-10-24 13:53:57 -0300

    Add files via upload

--- atividade1Logica.js
+++ atividade1Logica.js
@@ -1,41 +1,30 @@
 let nome = ["blaze", "gatsu", "monopoly"] 
 let exp = [10001, 10000, 1000]
+let categoria
 
 let i1 = 0
 let i2 = 0
 
 while (i1 < nome.length && i2 < exp.length) {    
     if (exp[i2] <= 1000) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Ferro.")
-        i1 += 1
-        i2 += 1
+        categoria = "Ferro"
     } else if ((exp[i2] > 1000) && (exp[i2] <= 2000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Bronze.")
-        i1 += 1
-        i2 += 1
+        categoria = "Bronze"
     } else if ((exp[i2] > 2000) && (exp[i2] <= 5000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Prata.")    
-        i1 += 1
-        i2 += 1
+        categoria = "Prata"    
     } else if ((exp[i2] > 5000) && (exp[i2] <= 7000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Ouro.")
-        i1 += 1
-        i2 += 1
+        categoria = "Ouro"
     } else if ((exp[i2] > 7000) && (exp[i2] <= 8000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Platina.")
-        i1 += 1
-        i2 += 1
+        categoria = "Platina"
     } else if ((exp[i2] > 8000) && (exp[i2] <= 9000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Ascendente.")
-        i1 += 1
-        i2 += 1
+        categoria = "Ascendente"
     } else if ((exp[i2] > 9000) && (exp[i2] <= 10000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Imortal.")
-        i1 += 1
-        i2 += 1
+        categoria = "Imortal"
     } else if ((exp[i2] > 10000)) {
-        console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria Radiante.")
-        i1 += 1
-        i2 += 1 
+        categoria = "Radiante"
     }
+
+    console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria " + categoria)
+    i1 += 1
+    i2++
 }
