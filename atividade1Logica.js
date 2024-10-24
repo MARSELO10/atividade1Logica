5cf1814ef31dd8c3f974ca499d493075078f77f9
MARCELO VIEIRA <104174604+MARSELO10@users.noreply.github.com>
2024-10-24 13:56:18 -0300

    Add files via upload

--- atividade1Logica.js
+++ atividade1Logica.js
@@ -2,29 +2,27 @@ let nome = ["blaze", "gatsu", "monopoly"]
 let exp = [10001, 10000, 1000]
 let categoria
 
-let i1 = 0
-let i2 = 0
+let i = 0
 
-while (i1 < nome.length && i2 < exp.length) {    
-    if (exp[i2] <= 1000) {
+while (i < nome.length && i < exp.length) {    
+    if (exp[i] <= 1000) {
         categoria = "Ferro"
-    } else if ((exp[i2] > 1000) && (exp[i2] <= 2000)) {
+    } else if ((exp[i] > 1000) && (exp[i] <= 2000)) {
         categoria = "Bronze"
-    } else if ((exp[i2] > 2000) && (exp[i2] <= 5000)) {
+    } else if ((exp[i] > 2000) && (exp[i] <= 5000)) {
         categoria = "Prata"    
-    } else if ((exp[i2] > 5000) && (exp[i2] <= 7000)) {
+    } else if ((exp[i] > 5000) && (exp[i] <= 7000)) {
         categoria = "Ouro"
-    } else if ((exp[i2] > 7000) && (exp[i2] <= 8000)) {
+    } else if ((exp[i] > 7000) && (exp[i] <= 8000)) {
         categoria = "Platina"
-    } else if ((exp[i2] > 8000) && (exp[i2] <= 9000)) {
+    } else if ((exp[i] > 8000) && (exp[i] <= 9000)) {
         categoria = "Ascendente"
-    } else if ((exp[i2] > 9000) && (exp[i2] <= 10000)) {
+    } else if ((exp[i] > 9000) && (exp[i] <= 10000)) {
         categoria = "Imortal"
-    } else if ((exp[i2] > 10000)) {
+    } else if ((exp[i] > 10000)) {
         categoria = "Radiante"
     }
 
-    console.log("O jogador " + nome[i1] + " tem nível " + exp[i2] + " e está na categoria " + categoria)
-    i1 += 1
-    i2++
+    console.log("O jogador " + nome[i] + " tem nível " + exp[i] + " e está na categoria " + categoria)
+    i += 1
 }
